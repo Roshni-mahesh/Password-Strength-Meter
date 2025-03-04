4d542d909c22ad1f5bd0569757764e8b375e798d
Roshni Mahesh <160133342+Roshni-mahesh@users.noreply.github.com>
2025-03-05 00:43:13 +0500

    Update psm.py

--- psm.py
+++ psm.py
@@ -10,7 +10,7 @@ st.markdown("""
     .main {text-align: center;}
     .stTextInput {width: 60% !important; margin: auto;}
     .stButton button {width: 50%; background-color: #4CAF50; color: white; font-size: 18px;}
-    .stButton button:hover {background-color: #45a049;}
+    .stButton button:hover {background-color: #45a049; center:center}
 </style>
 """, unsafe_allow_html=True)
 
